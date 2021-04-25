62c373c2465654969a3ac78fdf83ef66ba2cd380
Leah Zahn <zahn@chapman.edu>
2021-04-25 00:57:36 -0700

    working on dash

--- Assets/Scripts/PatrollerScript.cs
+++ Assets/Scripts/PatrollerScript.cs
@@ -8,10 +8,10 @@ public class PatrollerScript : MonoBehaviour
     public List<WaypointScript> Waypoints = new List<WaypointScript>();
     public float Speed = 1.0f;
     public int DestinationWaypoint = 1;
-    public GameObject upPOV;
-    public GameObject rightPOV;
-    public GameObject leftPOV;
-    public GameObject downPOV;
+    //public GameObject upPOV;
+    //public GameObject rightPOV;
+    //public GameObject leftPOV;
+    //public GameObject downPOV;
     public bool spottedPlayer = false;
 
     private Vector3 Destination;
@@ -32,14 +32,29 @@ public class PatrollerScript : MonoBehaviour
         StopAllCoroutines();
         StartCoroutine(MoveTo());
 
-        if (spottedPlayer)
+        if (CheckforPlayer())
         {
-            this.Destination = player.transform.position;
+            //this.Destination = player.transform.position;
             StopAllCoroutines();
-            StartCoroutine(MoveTo());
+            StartCoroutine(MoveToPlayer());
         }
 
         //if ((transform.position - this.Destination).sqrMagnitude > 1.30f)
+    }
+
+    private bool CheckforPlayer()
+    {
+        return ((this.transform.position - player.transform.position).sqrMagnitude < 7.0f);
+    }
+
+    IEnumerator MoveToPlayer()
+    {
+        while ((transform.position - player.transform.position).sqrMagnitude > 0.01f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,
+                player.transform.position, this.Speed * Time.deltaTime);
+            yield return null;
+        }
     }
 
     IEnumerator MoveTo()
@@ -53,47 +68,47 @@ public class PatrollerScript : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position,
                 this.Destination, this.Speed * Time.deltaTime);
 
-            float xDifference = this.Destination.x - transform.position.x;
-            float yDifference = this.Destination.y - transform.position.y;
+            //float xDifference = this.Destination.x - transform.position.x;
+            //float yDifference = this.Destination.y - transform.position.y;
 
-            if (Math.Abs(xDifference) > Math.Abs(yDifference))
-            {
-                if (xDifference > 0)
-                {
-                    rightPOV.gameObject.transform.position = new Vector3(this.transform.position.x + 0.75f, this.transform.position.y, 0);
-                    rightPOV.gameObject.SetActive(true);
-                    leftPOV.gameObject.SetActive(false);
-                    upPOV.gameObject.SetActive(false);
-                    downPOV.gameObject.SetActive(false);
-                }
-                else
-                {
-                    leftPOV.gameObject.transform.position = new Vector3(this.transform.position.x - 0.75f, this.transform.position.y, 0);
-                    leftPOV.gameObject.SetActive(true);
-                    rightPOV.gameObject.SetActive(false);
-                    upPOV.gameObject.SetActive(false);
-                    downPOV.gameObject.SetActive(false);
-                }
-            }
-            else
-            {
-                if (yDifference > 0)
-                {
-                    upPOV.gameObject.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 1.25f, 0);
-                    upPOV.gameObject.SetActive(true);
-                    leftPOV.gameObject.SetActive(false);
-                    rightPOV.gameObject.SetActive(false);
-                    downPOV.gameObject.SetActive(false);
-                }
-                else
-                {
-                    downPOV.gameObject.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 1.25f, 0);
-                    downPOV.gameObject.SetActive(true);
-                    leftPOV.gameObject.SetActive(false);
-                    upPOV.gameObject.SetActive(false);
-                    rightPOV.gameObject.SetActive(false);
-                }
-            }
+            //if (Math.Abs(xDifference) > Math.Abs(yDifference))
+            //{
+            //    if (xDifference > 0)
+            //    {
+            //        rightPOV.gameObject.transform.position = new Vector3(this.transform.position.x + 0.75f, this.transform.position.y, 0);
+            //        rightPOV.gameObject.SetActive(true);
+            //        leftPOV.gameObject.SetActive(false);
+            //        upPOV.gameObject.SetActive(false);
+            //        downPOV.gameObject.SetActive(false);
+            //    }
+            //    else
+            //    {
+            //        leftPOV.gameObject.transform.position = new Vector3(this.transform.position.x - 0.75f, this.transform.position.y, 0);
+            //        leftPOV.gameObject.SetActive(true);
+            //        rightPOV.gameObject.SetActive(false);
+            //        upPOV.gameObject.SetActive(false);
+            //        downPOV.gameObject.SetActive(false);
+            //    }
+            //}
+            //else
+            //{
+            //    if (yDifference > 0)
+            //    {
+            //        upPOV.gameObject.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 1.25f, 0);
+            //        upPOV.gameObject.SetActive(true);
+            //        leftPOV.gameObject.SetActive(false);
+            //        rightPOV.gameObject.SetActive(false);
+            //        downPOV.gameObject.SetActive(false);
+            //    }
+            //    else
+            //    {
+            //        downPOV.gameObject.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 1.25f, 0);
+            //        downPOV.gameObject.SetActive(true);
+            //        leftPOV.gameObject.SetActive(false);
+            //        upPOV.gameObject.SetActive(false);
+            //        rightPOV.gameObject.SetActive(false);
+            //    }
+            //}
 
             yield return null;
         }
@@ -147,8 +162,15 @@ public class PatrollerScript : MonoBehaviour
     {
         if (collision.gameObject.layer == 8)
         {
+            StopAllCoroutines();
             this.transform.position = this.Waypoints[DestinationWaypoint].transform.position;
             spottedPlayer = false;
         }
+        //else if (collision.gameObject.layer == 10)
+        //{
+        //    StopAllCoroutines();
+        //    this.transform.position = this.Waypoints[DestinationWaypoint].transform.position;
+        //    spottedPlayer = false;
+        //}
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,17 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
-    public float speed = 1f;
+    public float speed;
     public Animator animator;
     public GameObject startPoint;
     public GameObject KeyText;
+    public ButtonController buttonController;
 
     bool doubleClick = false;
     float timer = 0.0f;
     float doubleClickTimer = 0.0f;
+    bool hasKey = false;
+    bool speedDoubled = false;
 
     // Start is called before the first frame update
     void Start()
@@ -32,33 +35,52 @@ public class PlayerController : MonoBehaviour
             //StopAllCoroutines();
             StopCoroutine(Click());
             StopCoroutine(MoveTo(transform.position, mouseInSpace, speed));
+
+            mouseInSpace = new Vector3(mouseInSpace.x, mouseInSpace.y, 0);
+
             if ((timer > 0.0f) && !doubleClick)
             {
                 Debug.Log("dash");
                 doubleClick = true;
+                speed *= 2;
+                //if (!speedDoubled)
+                //{
+                //    speed *= 2;
+                //    speedDoubled = true;
+                //}
                 StartCoroutine(OnDoubleClick());
+
             }
             else
                 doubleClick = false;
 
-            mouseInSpace = new Vector3(mouseInSpace.x, mouseInSpace.y, 0);
+            
             StartCoroutine(Click());
 
             StartCoroutine(MoveTo(transform.position, mouseInSpace, speed));
 
         }
+
+        //if (speedDoubled)
+        //    speedDoubled = false;
     }
 
     IEnumerator OnDoubleClick()
     {
-        while (doubleClickTimer < 1.50f)
+        //speed *= 2;
+        while (doubleClickTimer < 5.0f)
         {
             doubleClickTimer += Time.deltaTime;
+            Debug.Log(doubleClickTimer + ", " + doubleClick);
+            doubleClick = true;
             yield return null;
         }
 
-        if (doubleClickTimer >= 1.50f)
+        if (doubleClickTimer >= 5.0f)
         {
+            Debug.Log("in reset speed if");
+            speed /= 2;
+            //speedDoubled = false;
             doubleClickTimer = 0.0f;
             doubleClick = false;
         }
@@ -84,10 +106,10 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetBool("isRunning", true);
 
-            if (doubleClick)
-                transform.position = Vector2.MoveTowards(transform.position, destination, speed * Time.deltaTime * 2);
-            else
-                transform.position = Vector2.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            //if (doubleClick)
+            //    transform.position = Vector2.MoveTowards(transform.position, destination, speed * Time.deltaTime * 2);
+            //else
+            transform.position = Vector2.MoveTowards(transform.position, destination, speed * Time.deltaTime);
             yield return true;
         }
 
@@ -99,7 +121,7 @@ public class PlayerController : MonoBehaviour
     {
         StopAllCoroutines();
 
-        Debug.Log("collision w/ " + collision.gameObject.layer);
+        //Debug.Log("collision w/ " + collision.gameObject.layer);
 
         if (collision.gameObject.layer == 10) //wall
         {
@@ -107,9 +129,14 @@ public class PlayerController : MonoBehaviour
         }
         else if (collision.gameObject.layer == 11) //door
         {
-            StopAllCoroutines();
+            if (hasKey)
+            {
+                buttonController.OnGameQuitButtonClick();
+                StopAllCoroutines();
+            }
+            
             Debug.Log("Door hit!");
-            Application.Quit();
+            
         }
         else if (collision.gameObject.layer == 9) //enemy
         {
@@ -119,6 +146,7 @@ public class PlayerController : MonoBehaviour
         {
             collision.gameObject.SetActive(false);
             KeyText.GetComponent<Text>().text = "Key: Acquired";
+            hasKey = true;
         }
     }
 }
